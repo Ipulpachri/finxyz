004d5c5b6b93d648074c75b33527213b10c925c4
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-03-03 21:02:53 +0700

    hentong.js

--- message/hentong.js
+++ message/hentong.js
@@ -2,69 +2,69 @@ exports.ahah = (prefix, pushname) => {
 return`
 𝗡𝗦𝗙𝗪 𝗠𝗘𝗡𝗨
 
- *╭─❒ ⌜ MENU NSFW ⌟*
- *│▸*
- *│▸* ${prefix}neko
- *│▸* ${prefix}waifu
- *│▸* ${prefix}trap
- *│▸* ${prefix}blowjob
- *│▸* ${prefix}yuri
- *│▸* ${prefix}kiss
- *│▸* ${prefix}cum
- *│▸* ${prefix}futanari
- *│▸* ${prefix}hentai
- *│▸* ${prefix}eroyuri
- *│▸* ${prefix}eron
- *│▸* ${prefix}erok
- *│▸* ${prefix}ero
- *│▸* ${prefix}solo
- *│▸* ${prefix}erokemo
- *│▸* ${prefix}fox_girl
- *│▸* ${prefix}feet
- *│▸* ${prefix}les
- *│▸* ${prefix}feed
- *│▸* ${prefix}bj
- *│▸*
+ *╭─❒ ⌜ MENU NSFW ⌟ ❒──*
+ *│*
+ *│✾* ${prefix}neko
+ *│✾* ${prefix}waifu
+ *│✾* ${prefix}trap
+ *│✾* ${prefix}blowjob
+ *│✾* ${prefix}yuri
+ *│✾* ${prefix}kiss
+ *│✾* ${prefix}cum
+ *│✾* ${prefix}futanari
+ *│✾* ${prefix}hentai
+ *│✾* ${prefix}eroyuri
+ *│✾* ${prefix}eron
+ *│✾* ${prefix}erok
+ *│✾* ${prefix}ero
+ *│✾* ${prefix}solo
+ *│✾* ${prefix}erokemo
+ *│✾* ${prefix}fox_girl
+ *│✾* ${prefix}feet
+ *│✾* ${prefix}les
+ *│✾* ${prefix}feed
+ *│✾* ${prefix}bj
+ *│*
  *╰────────────❒*
 					
 					
- *╭─❒ ⌜ MENU SFW ⌟*
- *│▸*
- *│▸* ${prefix}shinobu
- *│▸* ${prefix}megumin
- *│▸* ${prefix}bully
- *│▸* ${prefix}cuddle
- *│▸* ${prefix}cry
- *│▸* ${prefix}hug
- *│▸* ${prefix}awoo
- *│▸* ${prefix}kiss
- *│▸* ${prefix}lick
- *│▸* ${prefix}pat
- *│▸* ${prefix}smug
- *│▸* ${prefix}bonk
- *│▸* ${prefix}yeet
- *│▸* ${prefix}blush
- *│▸* ${prefix}smile
- *│▸* ${prefix}wave
- *│▸* ${prefix}highfive
- *│▸* ${prefix}handhold
- *│▸* ${prefix}nom
- *│▸* ${prefix}bite
- *│▸* ${prefix}glomp
- *│▸* ${prefix}slap
- *│▸* ${prefix}kill
- *│▸* ${prefix}happy
- *│▸* ${prefix}wink
- *│▸* ${prefix}waifunime              
- *│▸* ${prefix}baka
- *│▸* ${prefix}hug
- *│▸* ${prefix}kemonomimi
- *│▸* ${prefix}holo
- *│▸*
+ *╭─❒ ⌜ MENU SFW ⌟ ❒──*
+ *│*
+ *│✾* ${prefix}shinobu
+ *│✾* ${prefix}megumin
+ *│✾* ${prefix}bully
+ *│✾* ${prefix}cuddle
+ *│✾* ${prefix}cry
+ *│✾* ${prefix}hug
+ *│✾* ${prefix}awoo
+ *│✾* ${prefix}kiss
+ *│✾* ${prefix}lick
+ *│✾* ${prefix}pat
+ *│✾* ${prefix}smug
+ *│✾* ${prefix}bonk
+ *│✾* ${prefix}yeet
+ *│✾* ${prefix}blush
+ *│✾* ${prefix}smile
+ *│✾* ${prefix}wave
+ *│✾* ${prefix}highfive
+ *│✾* ${prefix}handhold
+ *│✾* ${prefix}nom
+ *│✾* ${prefix}bite
+ *│✾* ${prefix}glomp
+ *│✾* ${prefix}slap
+ *│✾* ${prefix}kill
+ *│✾* ${prefix}happy
+ *│✾* ${prefix}wink
+ *│✾* ${prefix}waifunime              
+ *│✾* ${prefix}baka
+ *│✾* ${prefix}hug
+ *│✾* ${prefix}kemonomimi
+ *│✾* ${prefix}holo
+ *│*
  *╰────────────❒*
 
 
- *╭─❒ ⌜ THANKS TO ⌟*
+ *╭─❒ ⌜ THANKS TO ⌟ ❒──*
  *│*
  *│* ➤ Fachri
  *│* ➤ Dittaz
