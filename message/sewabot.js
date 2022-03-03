df4e9ee8c20ef5727e763667f7c9660cfb9d41ff
SF-BOTZ <94295494+Ipulpachri@users.noreply.github.com>
2022-03-03 11:15:14 +0700

    sewabot.js

--- message/sewabot.js
+++ message/sewabot.js
@@ -1,33 +1,33 @@
 
 exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
 return `${ucapanWaktu} kak ${pushname}
-Berikut adalah list harga untuk sewa botz
+Berikut adalah list harga untuk sewa Fachri Botz
 
-*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*╭─❒*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
 *┴*
 *│▸* Seminggu : 5K         
 *│▸* 1 Bulan = 10K        
 *│▸* permanen = 20K          
 *|-* Run Bot = 30K + Jadi Owner             
 *┬*
-*╰────────────[*
+*╰────────────❒*
 
-*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*╭─❒*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
 *┴*
 *│▸* Shopee : ${settings.Shopee}
 *│▸* Gopay : ${settings.Gopay}
 *│▸* Dana : ${settings.Dana}
 *│▸* Ovo : ${settings.Ovo}
 *┬*
-*╰────────────[*
+*╰────────────❒*
 
 𝗡𝗢𝗧𝗘 : 
 
-Bot on 24 jam tapi kadang 
+❒ Bot on 24 jam tapi kadang 
 juga mati klo lgi ada error 
 atau lgi perbaikan bug.
 
-Kalo mau SC botnya bisa chat owner !!,
+❒ Kalo mau SC botnya bisa chat owner !!,
 `
 }
 
